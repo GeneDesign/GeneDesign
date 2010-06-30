18bd039c0f17e677bf9d87a0b0039ffd883b62db
Steffi Liu <steffi@steffi-laptop.(none)>
2010-06-30 16:11:38 -0400

    This is the command line version of gdSSREM

---
+++ bin/Restriction_Site_Subtraction.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+use Getopt::Long;
+use File::Basename qw(fileparse);
+use File::Path qw(make_path);
+use Perl6::Slurp;
+use GeneDesign;
+
+my %ORGNAME = %ORGANISMS;
+my $CODON_TABLE = define_codon_table(1);
+my $RE_DATA = define_sites($enzfile);
+
+
+##Get Arguments
+my %config = ();
+GetOptions (
+  	'input=s'		=> \$config{INPUT},
+	'organism=i'    	=> \$config{ORGANISM},
+	'sites=s'		=> \$config{RESTRICTION_SITES},
+	'help'			=> \$config{HELP},
+        'repeat=i'              => \$config{ITERATIONS},
+    );
+
+##Respond to cries for help
+if ($config{HELP}) {
+    print "
+Restriction_Site_Subtraction.pl
+    
+    
+    
+    
+    
+    
+";
+    exit;
+}
+
+##Check the consistency of arguments
+die "\n ERROR: $config{INPUT} does not exist!\n"
+    if (! -e $config{INPUT});
+die "\n ERROR: $config{RESTRICTION_SITES} does not exist!\n"
+    if (! -e $config{RESTRICTION_SITES});
+die "\n ERROR: An organism was not supplied\n"
+    if (! $config{ORGANISM});
+warn "\n ERROR: Number of iterations was not supplied\n"
+    if (! $config{ITERATIONS});
+warn "\n WARNING: $_ is not a recognized organism and will be ignored.\n"
+    foreach (grep {! exists($ORGANISMS{$_})} split ("", $config{ORGANISM}) );
+
+
+##Fetch input nucleotide sequences, organisms, iterations, and restriction site file
+my $filename  	  	= fileparse( $config{INPUT}, qr/\.[^.]*/);
+make_path($filename . "_gdRSS");
+
+my $input    	  	= slurp( $config{INPUT} );
+my $nucseq 	        = fasta_parser( $input );
+
+my @ORGSDO		= grep { exists $ORGANISMS{$_} } split( "", $config{ORGANISM} );
+
+my @remove_RE           = slurp( $config{RESTRICTION_SITES} ) ;
+chomp (@remove_RE);
+
+my $iter                = $config{ITERATIONS} ? $config{ITERATIONS} : 3;
+
+print "\n";
+
+
+##Finally subtracts restriction enzymes
+
+foreach my $org (@ORGSDO)
+{
+    my $OUTPUT = {};
+    my ($Error4, $Error0) = ("", "");
+    
+    foreach my $seqkey (sort {$a cmp $b} keys %$nucseq)
+    {
+        my $newnuc = $$nucseq{$seqkey};
+    	for (1..$iter) ##Where the magic happens
+	{
+            foreach my $enz (@remove_RE)
+            {	
+                my $temphash = siteseeker($newnuc, $enz, $$RE_DATA{REGEX}->{$enz});
+                foreach my $grabbedpos (keys %$temphash)
+                {
+                    my $grabbedseq = $$temphash{$grabbedpos};
+                    my $framestart = ($grabbedpos) % 3;
+                    my $critseg = substr($newnuc, $grabbedpos - $framestart, ((int(length($grabbedseq)/3 + 2))*3));
+                    my $newcritseg = pattern_remover($critseg, $$RE_DATA{CLEAN}->{$enz}, $CODON_TABLE, define_RSCU_values($org));
+                    substr($newnuc, $grabbedpos - $framestart, length($newcritseg)) = $newcritseg if (scalar( keys %{siteseeker($newcritseg, $enz, $$RE_DATA{REGEX}->{$enz})}) == 0);
+                }
+            }
+	}
+        my $new_key = $seqkey . " after the restriction site subtraction algorithm for $ORGNAME{$org}";
+        $$OUTPUT{$new_key} = $newnuc;
+        
+        my @success_enz = ();
+        my @fail_enz = ();
+        
+	foreach my $enz (@remove_RE) #Stores successfully and unsuccessfully removed enzymes in respective arrays
+	{
+            my $checkpres = siteseeker($newnuc, $enz, $$RE_DATA{REGEX}->{$enz});
+            push @fail_enz, $enz if (scalar(keys %$checkpres) != 0);
+            push @success_enz, $enz if (scalar (keys %$checkpres) == 0);
+	}
+        
+        $Error4 = "I was unable to remove @fail_enz after $iter iterations." if @fail_enz;
+        $Error0 = "I successfully removed @success_enz from your sequence.";
+        
+        my $newal = compare_sequences($$nucseq{$seqkey}, $newnuc);
+	my $bcou = count($newnuc);
+        
+        print "
+For the sequence $new_key:
+    I was asked to remove: @remove_RE.
+    $Error4
+    $Error0
+
+    Base Count: $$bcou{length} bp ($$bcou{A} A, $$bcou{T} T, $$bcou{C} C, $$bcou{G} G)
+    Composition : $$bcou{GCp}% GC, $$bcou{ATp}% AT
+    $$newal{'I'} Identities, $$newal{'D'} Changes ($$newal{'T'} transitions, $$newal{'V'} transversions), $$newal{'P'}% Identity
+        
+        
+"
+    }
+            
+        open (my $fh, ">" . $filename . "_gdRSS/" . $filename . "_gdRSS_$org.FASTA") || die "Cannot create output file, $!";
+        print $fh fasta_writer($OUTPUT);
+        close $fh;
+        print $filename . "_gdRSS_$org.FASTA has been written.\n" 
+}
+
+print "\n";
+
+exit;
