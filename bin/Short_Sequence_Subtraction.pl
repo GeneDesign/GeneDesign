d5fed7e63adb3347ce6700072e55666319e886df
Steffi Liu <steffi@steffi-laptop.(none)>
2010-07-09 15:39:53 -0400

    Changed a minor typo in the Restriction_Site file, adding Short_Sequence_Subtraction file and test short_sequences txt file.

--- bin/Short_Sequence_Subtraction.pl
+++ bin/Short_Sequence_Subtraction.pl
@@ -10,8 +10,6 @@ use GeneDesign;
 
 my %ORGNAME = %ORGANISMS;
 my $CODON_TABLE = define_codon_table(1);
-my $RE_DATA = define_sites($enzfile);
-
 
 ##Get Arguments
 my %config = ();
@@ -19,36 +17,37 @@ GetOptions (
   	'input=s'		=> \$config{INPUT},
 	'organism=i'    	=> \$config{ORGANISM},
         'rscu=s'                => \$config{RSCU_FILE},
-	'sites=s'		=> \$config{RESTRICTION_SITES},
+	'sequences=s'		=> \$config{SHORT_SEQUENCE},
         'times=i'               => \$config{ITERATIONS},
  	'help'			=> \$config{HELP},
    );
 
 ##Respond to cries for help
 if ($config{HELP}) {
-    print "
-Restriction_Site_Subtraction.pl
-    
-    Given at least one nucleotide sequence as input, the
-    Restriction_Site_Subtraction script searches through the sequence for
-    targeted restriction enzymes and attempts to remove the sites from the
+    print "   
+Short_Sequence_Subtraction.pl
+
+     Given at least one nucleotide sequence as input, the
+    Short_Sequence_Subtraction script searches through the sequence for
+    targeted short sequences and attempts to remove the target sites from the
     sequence without changing the amino acid sequence by changing whole codons.
     
     The targeted codons may be replaced with random codons, using a user-defined
     codon table, or using the default codon table for a user-selected organism.
     
     Output will be named according to the name of the FASTA input file and will
-    be tagged with the gdRSS suffix and the number of the organism used (or 0 if
+    be tagged with the gdSSS suffix and the number of the organism used (or 0 if
     a custom RSCU table was provided or 7 if codons were replaced randomly).
     
   Usage examples:
-   perl Restriction_Site_Subtraction.pl -i Test_YAR000W.FASTA -o 13 -s sites.txt
-    ./ Restriction_Site_Subtraction.pl --input Test_YAR000W.FASTA --rscu Test_
-TY1_RSCU.txt --times 6 --sites sites.txt
+   perl Short_Sequence_Subtraction.pl -i Test_YAR000W.FASTA -o 13 -s
+short_sequences.txt
+    ./ Short_Sequence_Subtraction.pl --input Test_YAR000W.FASTA --rscu Test_
+TY1_RSCU.txt --times 6 --sites short_sequences.txt
 
  Required arguments:
     -i,   --input : a FASTA file containing nucleotide sequences.
-    -s,   --sites : the restriction enzymes sites that will be silently removed
+    -s,   --sequences : the short sequences that will be silently removed
 
   Optional arguments:
     -h,   --help : Display this message
@@ -63,7 +62,6 @@ TY1_RSCU.txt --times 6 --sites sites.txt
     another organism, named after the table's filename. If neither are given,
     then the script will replace the sites with random codons.
     
-    
 ";
     exit;
 }
@@ -71,8 +69,8 @@ TY1_RSCU.txt --times 6 --sites sites.txt
 ##Check the consistency of arguments
 die "\n ERROR: The input file does not exist!\n"
     if (! -e $config{INPUT});
-die "\n ERROR: You did not provide a restriction sites file!\n"
-    if (! -e $config{RESTRICTION_SITES});
+die "\n ERROR: You did not provide a short sequence file!\n"
+    if (! -e $config{SHORT_SEQUENCE});
 
 warn "\n ERROR: Your RSCU file does not exist! Your target codons will be replaced by random codons.\n"
     if ($config{RSCU_FILE} && ! -e $config{RSCU_FILE});
@@ -82,12 +80,12 @@ warn "\n ERROR: Number of iterations was not supplied. The default number of 3 w
     if (! $config{ITERATIONS});
 warn "\n WARNING: $_ is not a recognized organism and will be ignored.\n"
     foreach (grep {! exists($ORGANISMS{$_})} split ("", $config{ORGANISM}) );
+    
 
-
-##Fetch input nucleotide sequences, organisms, RSCU file, iterations, and restriction site file
+##Fetch input nucleotide sequences, organisms, RSCU file, iterations, and short sequences file
 
 my $filename  	  	= fileparse( $config{INPUT}, qr/\.[^.]*/);
-make_path($filename . "_gdRSS");
+make_path($filename . "_gdSSS");
 
 my $input    	  	= slurp( $config{INPUT} );
 my $nucseq 	        = fasta_parser( $input );
@@ -105,16 +103,20 @@ if (! $config{ORGANISM} && ! $config{RSCU_FILE}) {
     $ORGNAME{7}         = 'random codons';
 }
 
-my @remove_RE           = slurp( $config{RESTRICTION_SITES} ) ;
-chomp (@remove_RE);
+my @shortseq           = slurp( $config{SHORT_SEQUENCE} ) ;
+chomp (@shortseq);
 
 my $iter                = $config{ITERATIONS}   ?   $config{ITERATIONS}     : 3;
 
+## More consistency checking
+foreach my $seq (@shortseq) {
+    die "\n ERROR: You need a short sequence to be removed (at least 2 bp).\n" 
+        if (length($seq) < 2);
+}
+
 print "\n";
 
-
-##Finally subtracts restriction enzymes
-
+#Finally removes short sequences
 foreach my $org (@ORGSDO)
 {
     my $OUTPUT = {};
@@ -122,59 +124,71 @@ foreach my $org (@ORGSDO)
     
     foreach my $seqkey (sort {$a cmp $b} keys %$nucseq)
     {
-        my $newnuc = $$nucseq{$seqkey};
-        my ($Error4, $Error0, $Error5) = ("", "", "");
-        my @success_enz = ();
-        my @fail_enz = ();
-        my @none_enz = ();
+        
+        my $oldnuc = $$nucseq{$seqkey};
+        my $newnuc = $oldnuc;
+        my ($Error4, $Error0, $Error5, $Error6) = ("", "", "", "");
+        my @success_seq = ();
+        my @fail_seq = ();
+        my @none_seq = ();
       
     	for (1..$iter) ##Where the magic happens
 	{
-            foreach my $enz (@remove_RE)
-            {	
-                my $temphash = siteseeker($newnuc, $enz, $$RE_DATA{REGEX}->{$enz});
+	    my $warncount = 0;
+            foreach my $remseq (@shortseq)
+            {
+		if (length($remseq) >= length($newnuc)) { ## And yet more consistency checking
+		    warn "\n ERROR: Your short sequence should be shorter than your nucleotide sequence!\n" if ($warncount == 0);
+		    $warncount++;
+		    last;
+		}
+		$remseq = cleanup($remseq, 0);
+                my $arr = [ regres($remseq, 1) ];
+                my $temphash = siteseeker($newnuc, $remseq, $arr);
                 foreach my $grabbedpos (keys %$temphash)
                 {
                     my $grabbedseq = $$temphash{$grabbedpos};
-                    print $grabbedseq;
                     my $framestart = ($grabbedpos) % 3;
                     my $critseg = substr($newnuc, $grabbedpos - $framestart, ((int(length($grabbedseq)/3 + 2))*3));
                     my $newcritseg;
                     if (%$RSCU_VALUES) {
-                        $newcritseg = pattern_remover($critseg, $$RE_DATA{CLEAN}->{$enz}, $CODON_TABLE, $RSCU_VALUES);
+                        $newcritseg = pattern_remover($critseg, $arr, $CODON_TABLE, $RSCU_VALUES);
                     }
                     elsif (!%$RSCU_VALUES) {
-                        $newcritseg = random_pattern_remover($critseg, $$RE_DATA{CLEAN}->{$enz}, $CODON_TABLE);
+                        $newcritseg = random_pattern_remover($critseg, $arr, $CODON_TABLE);
                     }
-                    substr($newnuc, $grabbedpos - $framestart, length($newcritseg)) = $newcritseg if (scalar( keys %{siteseeker($newcritseg, $enz, $$RE_DATA{REGEX}->{$enz})}) == 0);
+                    substr($newnuc, $grabbedpos - $framestart, length($newcritseg)) = $newcritseg if (scalar( keys %{siteseeker($newcritseg, $remseq, $arr)}) == 0);
                 }
             }
 	}
-        my $new_key = $seqkey . " after the restriction site subtraction algorithm for $ORGNAME{$org}";
+        my $new_key = $seqkey . " after the short sequence subtraction algorithm for $ORGNAME{$org}";
         $$OUTPUT{$new_key} = $newnuc;
         
-	foreach my $enz (@remove_RE) #Stores successfully and unsuccessfully removed enzymes in respective arrays
+	foreach my $remseq (@shortseq) #Stores successfully and unsuccessfully removed sequences in respective arrays
 	{
-            my $oldcheckpres = siteseeker($$nucseq{$seqkey}, $enz, $$RE_DATA{REGEX}->{$enz});
-            my $newcheckpres = siteseeker($newnuc, $enz, $$RE_DATA{REGEX}->{$enz});
-            push @none_enz, $enz if scalar(keys %$oldcheckpres == 0);
-            push @fail_enz, $enz if (scalar(keys %$newcheckpres) != 0);
-            push @success_enz, $enz if (scalar (keys %$newcheckpres) == 0 && scalar (keys %$oldcheckpres != 0));
+            my $arr = [ regres($remseq, 1) ];
+            my $oldcheckpres = siteseeker($$nucseq{$seqkey}, $remseq, $arr);
+            my $newcheckpres = siteseeker($newnuc, $remseq, $arr);
+            push @none_seq, $remseq if scalar(keys %$oldcheckpres == 0);
+            push @fail_seq, $remseq if (scalar(keys %$newcheckpres) != 0);
+            push @success_seq, $remseq if (scalar (keys %$newcheckpres) == 0 && scalar (keys %$oldcheckpres != 0));
 	}
         
-        $Error4 = "I was unable to remove @fail_enz after $iter iterations." if @fail_enz;
-        $Error0 = "I successfully removed @success_enz from your sequence." if @success_enz;
-        $Error5 = "There were no instances of @none_enz present in your sequence." if @none_enz;
+        $Error6 = "The translation has changed!" if (translate($oldnuc, 1, $CODON_TABLE) ne translate($newnuc, 1, $CODON_TABLE));        
+        $Error4 = "I was unable to remove @fail_seq after $iter iterations." if @fail_seq;
+        $Error0 = "I successfully removed @success_seq from your sequence." if @success_seq;
+        $Error5 = "There were no instances of @none_seq present in your sequence." if @none_seq;
         
         my $newal = compare_sequences($$nucseq{$seqkey}, $newnuc);
 	my $bcou = count($newnuc);
         
         print "
 For the sequence $new_key:
-    I was asked to remove: @remove_RE.
+    I was asked to remove: @shortseq.
     $Error5
     $Error4
     $Error0
+    $Error6
         Base Count: $$bcou{length} bp ($$bcou{A} A, $$bcou{T} T, $$bcou{C} C, $$bcou{G} G)
         Composition : $$bcou{GCp}% GC, $$bcou{ATp}% AT
         $$newal{'I'} Identities, $$newal{'D'} Changes ($$newal{'T'} transitions, $$newal{'V'} transversions), $$newal{'P'}% Identity
@@ -182,10 +196,10 @@ For the sequence $new_key:
 "
     }
             
-    open (my $fh, ">" . $filename . "_gdRSS/" . $filename . "_gdRSS_$org.FASTA") || die "Cannot create output file, $!";
+    open (my $fh, ">" . $filename . "_gdSSS/" . $filename . "_gdSSS_$org.FASTA") || die "Cannot create output file, $!";
     print $fh fasta_writer($OUTPUT);
     close $fh;
-    print $filename . "_gdRSS_$org.FASTA has been written.\n"
+    print $filename . "_gdSSS_$org.FASTA has been written.\n"
 }
 
 print "\n";
