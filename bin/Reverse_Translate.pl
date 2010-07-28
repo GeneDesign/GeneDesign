75be34b9bbf6e096d17bbf8284d5780fb6d33f85
Steffi Liu <steffi@steffi-laptop.(none)>
2010-07-28 14:24:11 -0400

    Error message breaks code :[

--- bin/Reverse_Translate.pl
+++ bin/Reverse_Translate.pl
@@ -20,7 +20,8 @@ GetOptions (
 			'input=s'		=> \$config{INPUT},
 			'rscu=s'		=> \$config{RSCU_FILE},
 			'organism=i'		=> \$config{ORGANISM},
-			'help'			=> \$config{HELP}
+			'help'			=> \$config{HELP},
+			'table'			=> \$config{TABLE},
 		   );
 
 
@@ -46,11 +47,13 @@ Reverse_Translation.pl
     -i,   --input : a FASTA file containing protein sequences.
     -r,   --rscu : a txt file containing an RSCU table from gen_RSCU.pl
     -o,   --organism : at least one organism number.
+    -t,   --table : a table of codons
         Each organism given represents another iteration the algorithm must run.
         (1 = S.cerevisiae,  2 = E.coli,         3 = H.sapiens,
          4 = C.elegans,     5 = D.melanogaster, 6 = B.subtilis)
-    -r OR -o must be provided. If both are given the table will be treated as
-        another organism, named after the table's filename.
+    -r, -t, OR -o must be provided. If any combinations of these are given,the
+	codon table and/or rscu table will be treated as another organism, named
+	after the filename(s).
 
   Optional arguments:
     -h,   --help : Display this message
@@ -62,12 +65,14 @@ Reverse_Translation.pl
 
 
 ##Check the consistency of arguments
-die "\n ERROR: Neither an organism nor an RSCU table were supplied.\n"
-	if (! $config{ORGANISM} && ! $config{RSCU_FILE});
+die "\n ERROR: Neither an organism, an RSCU table, nor a codon table were supplied.\n"
+	if (! $config{ORGANISM} && ! $config{RSCU_FILE} && ! $config{TABLE});
 die "\n ERROR: $config{INPUT} does not exist.\n"
 	if (! -e $config{INPUT});
 die "\n ERROR: $config{RSCU_FILE} does not exist.\n"
 	if ($config{RSCU_FILE} && ! -e $config{RSCU_FILE});
+die "\n ERROR: $config{TABLE} does not exist.\n"
+	if ($config{TABLE} && ! -e $config{TABLE});
 warn "\n WARNING: $_ is not a recognized organism and will be ignored.\n"
 	foreach (grep {! exists($ORGANISMS{$_})} split ("", $config{ORGANISM}) );
 
@@ -75,16 +80,32 @@ warn "\n WARNING: $_ is not a recognized organism and will be ignored.\n"
 ##Fetch input sequences, RSCU table, organisms
 my $filename	  = fileparse( $config{INPUT}, qr/\.[^.]*/);
 make_path($filename . "_gdRT");
-my $input		  = slurp( $config{INPUT} );
+my $input	  = slurp( $config{INPUT} );
 my $ORIG_SEQUENCE = fasta_parser( $input );
 
-my $rscu		  = $config{RSCU_FILE}	?	slurp( $config{RSCU_FILE} )	:	0;
-my $RSCU_DEFN	  = $rscu				?	rscu_parser( $rscu )		:	{};
+my $rscu	  = $config{RSCU_FILE}	?	slurp( $config{RSCU_FILE} )	:	0;
+my $RSCU_DEFN	  = $rscu		?	rscu_parser( $rscu )		:	{};
+
+my $table	  = $config{TABLE}	?	slurp( $config{RSCU_FILE} )	:	0;
+my %codon_scheme;
+if ($table)
+{
+	my @pair = split(/[\n]/g, $table);
+	foreach my $entry (@pair) {
+		my $id = shift @pair;
+		$codon_scheme{$id} = shift @pair;
+	}
+}
+else
+{
+	%codon_scheme = ();
+}
 	
-my @ORGSDO		  = grep { exists $ORGANISMS{$_} }
-					split( "", $config{ORGANISM} );
+my @ORGSDO	  = grep { exists $ORGANISMS{$_} } split( "", $config{ORGANISM} );
 push @ORGSDO, 0	  if ($rscu);
-$ORGNAME{0}		  = fileparse( $config{RSCU_FILE}, qr/\.[^.]*/) if ($rscu);
+push @ORGSDO, 8   if ($table);
+$ORGNAME{0}	  = fileparse( $config{RSCU_FILE}, qr/\.[^.]*/) if ($rscu);
+$ORGNAME{8}	  = fileparse( $config{TABLE}, qr/\.[^.]*/) if ($table);
 
 print "\n";
 
@@ -93,12 +114,20 @@ foreach my $org (@ORGSDO)
 {
 	my $OUTPUT = {};
 	my $RSCU_VALUES = $org	?	define_RSCU_values( $org )	:	$RSCU_DEFN;
-	my %defaults = define_aa_defaults($CODON_TABLE, $RSCU_VALUES);
+	%codon_scheme = $table 	? 	%codon_scheme 			:       define_aa_defaults($CODON_TABLE, $RSCU_VALUES);
+	print "$_, $codon_scheme{$_}\n" foreach (sort keys %codon_scheme);
+	my $ERROR1 = "";
 	foreach my $seqkey (keys %$ORIG_SEQUENCE)
 	{
-		my $new_seq = reverse_translate( $$ORIG_SEQUENCE{$seqkey}, \%defaults );
+		my $new_seq = reverse_translate( $$ORIG_SEQUENCE{$seqkey}, \%codon_scheme );
 		my $new_key = $seqkey . " after the reverse translate algorithm, using $ORGNAME{$org} RSCU values";
 		$$OUTPUT{$new_key} = $new_seq;
+		if (length($$ORIG_SEQUENCE{$seqkey}) < length($new_seq)/3 || $$ORIG_SEQUENCE{$seqkey} ne translate($new_seq, 1, $CODON_TABLE))
+		{
+			my $errout = "\n" . $$ORIG_SEQUENCE{$seqkey} . "\n" . translate($new_seq, 1, $CODON_TABLE) . "\nCodons:\n";
+			$errout .= " $_, $codon_scheme{$_}\n" foreach (sort keys %codon_scheme);
+			warn "\nI was unable to reverse translate your sequence.  Perhaps you left something out of your codon table?\n$errout\n\n";
+		}
 	}
 	open (my $fh, ">" . $filename . "_gdRT/" . $filename. "_gdRT_$org.FASTA") || die "can't create output file, $!";
 	print $fh fasta_writer($OUTPUT);
